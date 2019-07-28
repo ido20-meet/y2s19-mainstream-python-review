bb53090bc0b320fd242e894dc730bc40bce75fd7
myname21-meet <myname21@meet.mit.edu>
2019-07-28 16:13:53 +0300

    lonlyyyyyyyyyyyyyyyyyyyyyyy im mr. lonlyyyyyyyyyyyyyyyyyyyy

--- part2.py
+++ part2.py
@@ -1,7 +1,14 @@
 # Part 2 of the Python Review lab.
 
 def encode(x, y):
-	pass
+	if 1<y<250 and 500<x<1000:
+		print(x * y)
+	else:
+		print("Invalid input: Outside range")
+		print("none")
 
 def decode(coded_message):
 	pass
+
+
+encode(100, 700)
